38ced84ad2893c65b0d31203d3b9eabbc1f8fa74
mlange-42 <martin_lange_@gmx.net>
2021-12-12 01:58:59 +0100

    replace ico sphere generation with own version, which does not generate duplicate nodes

--- scripts/geom/ico_sphere.gd
+++ scripts/geom/ico_sphere.gd
@@ -1,98 +1,185 @@
 class_name IcoSphere
 
-var functions = preload("functions.gd").new()
+var verbose = ProjectSettings.get_setting("debug/settings/stdout/verbose_stdout")
 
-var _subdivisions: int
-var _radius: float
-var _smooth: bool
-var _tool: SurfaceTool
+var subdivisions: int
+var radius: float
+var smooth: bool
+
+var vertices: PoolVector3Array
 
 
+# warning-ignore:shadowed_variable
+# warning-ignore:shadowed_variable
+# warning-ignore:shadowed_variable
 func _init(subdivisions: int, radius: float = 1, smooth = true):
-	self._subdivisions = subdivisions
-	self._radius = radius
-	self._smooth = smooth
-	self._tool = SurfaceTool.new()
+	self.subdivisions = subdivisions
+	self.radius = radius
+	self.smooth = smooth
 
 
 func create() -> Mesh:
-	_tool.begin(Mesh.PRIMITIVE_TRIANGLES)
+	var cache := Dictionary()
 	
-	if _smooth:
-		_tool.add_smooth_group(true)
+	var verts = PoolVector2Array()
+	verts.push_back(Vector2(  0, -58.5))
+	verts.push_back(Vector2(  0,  58.5))
+	verts.push_back(Vector2(180,  58.5))
+	verts.push_back(Vector2(180, -58.5))
 	
-	var lla1 = Vector3(0, -58.5, 0)
-	var lla2 = Vector3(0, 58.5, 0)
-	var lla3 = Vector3(180, 58.5, 0)
-	var lla4 = Vector3(180, -58.5, 0)
-	var lla5 = Vector3(90, -31.5, 0)
-	var lla6 = Vector3(90, 31.5, 0)
-	var lla7 = Vector3(-90, 31.5, 0)
-	var lla8 = Vector3(-90, -31.5, 0)
-	var lla9 = Vector3(-31.5, 0, 0)
-	var lla10 = Vector3(31.5, 0, 0)
-	var lla11 = Vector3(148.5, 0, 0)
-	var lla12 = Vector3(-148.5, 0, 0)
-
-	_create_mesh(lla2, lla3, lla7)
-	_create_mesh(lla2, lla6, lla3)
-	_create_mesh(lla6, lla11, lla3)
-	_create_mesh(lla3, lla11, lla12)
-	_create_mesh(lla3, lla12, lla7)
-	_create_mesh(lla8, lla7, lla12)
-	_create_mesh(lla9, lla7, lla8)
-	_create_mesh(lla9, lla2, lla7)
-	_create_mesh(lla10, lla2, lla9)
-	_create_mesh(lla10, lla6, lla2)
-	_create_mesh(lla10, lla5, lla6)
-	_create_mesh(lla5, lla11, lla6)
-	_create_mesh(lla11, lla5, lla4)
-	_create_mesh(lla11, lla4, lla12)
-	_create_mesh(lla12, lla4, lla8)
-	_create_mesh(lla1, lla9, lla8)
-	_create_mesh(lla1, lla10, lla9)
-	_create_mesh(lla5, lla10, lla1)
-	_create_mesh(lla5, lla1, lla4)
-	_create_mesh(lla4, lla1, lla8)
+	verts.push_back(Vector2( 90, -31.5))
+	verts.push_back(Vector2( 90,  31.5))
+	verts.push_back(Vector2(-90,  31.5))
+	verts.push_back(Vector2(-90, -31.5))
 	
-	if _smooth:
-		_tool.generate_normals()
+	verts.push_back(Vector2( -31.5, 0))
+	verts.push_back(Vector2(  31.5, 0))
+	verts.push_back(Vector2( 148.5, 0))
+	verts.push_back(Vector2(-148.5, 0))
 	
-	var mesh: Mesh = _tool.commit()
+	vertices = lla_to_xyz_arr(verts)
+	
+	var indices = PoolIntArray([
+		1, 2, 6,
+		1, 5, 2,
+		5, 10, 2,
+		2, 10, 11,
+		2, 11, 6,
+		7, 6, 11,
+		8, 6, 7,
+		8, 1, 6,
+		9, 1, 8,
+		9, 5, 1,
+		9, 4, 5,
+		4, 10, 5,
+		10, 4, 3,
+		10, 3, 11,
+		11, 3, 7,
+		0, 8, 7,
+		0, 9, 8,
+		4, 9, 0,
+		4, 0, 3,
+		3, 0, 7
+	])
+	
+	for _i in range(subdivisions):
+		var indices_subdiv = PoolIntArray()
+		for j in range(0, indices.size(), 3):
+			var j1 = indices[j]
+			var j2 = indices[j+1]
+			var j3 = indices[j+2]
+			
+			var v1 = middle_point(j1, j2, cache)
+			var v2 = middle_point(j2, j3, cache)
+			var v3 = middle_point(j3, j1, cache)
+			
+			indices_subdiv.append_array([j1, v1, v3])
+			indices_subdiv.append_array([j2, v2, v1])
+			indices_subdiv.append_array([j3, v3, v2])
+			indices_subdiv.append_array([v1, v2, v3])
+			
+		indices = indices_subdiv
+	
+	var normals = PoolVector3Array(vertices)
+	var uv = calc_uv_lla(vertices)
+	
+	# scale vertices to radius
+	if radius != 1.0:
+		scale_vertices(radius)
+	
+	var arr = []
+	arr.resize(Mesh.ARRAY_MAX)
+	arr[Mesh.ARRAY_VERTEX] = vertices
+	arr[Mesh.ARRAY_NORMAL] = normals
+	arr[Mesh.ARRAY_TEX_UV] = uv
+	arr[Mesh.ARRAY_TEX_UV2] = PoolVector2Array(uv)
+	arr[Mesh.ARRAY_INDEX] = indices
+	
+	var mesh = ArrayMesh.new()
+	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arr)
+	
+	if not smooth:
+		split_unsmooth(mesh)
+	
+	if verbose:
+		var arrs = mesh.surface_get_arrays(0)
+		print("Generated Ico Sphere with %d vertices and %d faces" 
+				% [arrs[Mesh.ARRAY_VERTEX].size(), arrs[Mesh.ARRAY_INDEX].size() / 3])
 	
 	return mesh
-	
-	
-func _create_tri(corner1: Vector3, corner2: Vector3, corner3: Vector3, 
-		lla1: Vector3, lla2: Vector3, lla3: Vector3):
-	var corners = [corner1, corner2, corner3]
-	var lla = [lla1, lla2, lla3]
-	
-	if not _smooth:
-		_tool.add_normal(-functions.calc_surface_normal_newell_method(corners))
-		
-	for i in range(corners.size()):
-		var ll: Vector3 = lla[i]
-		_tool.add_uv(Vector2(ll.x / 360.0 + 0.5, ll.y / 180.0 + 0.5))
-		_tool.add_uv2(Vector2(ll.x / 360.0 + 0.5, ll.y / 180.0 + 0.5))
-		_tool.add_vertex(corners[i])
 
 
-func _create_mesh(lla1, lla2, lla3, curr_division = _subdivisions):
-		if curr_division == 0:
-			var corner1 = functions.lla_to_xyz(lla1, _radius)
-			var corner2 = functions.lla_to_xyz(lla2, _radius)
-			var corner3 = functions.lla_to_xyz(lla3, _radius)
-			_create_tri(corner1, corner2, corner3, lla1, lla2, lla3)
-		else:
-			var corner1 = lla1
-			var corner2 = functions.mid_point(lla1, lla2)
-			var corner3 = lla2
-			var corner4 = functions.mid_point(lla2, lla3)
-			var corner5 = lla3
-			var corner6 = functions.mid_point(lla3, lla1)
-			var corners = [corner1, corner2, corner3, corner4, corner5, corner6]
-			var order = [[1, 2, 6], [2, 4, 6], [2, 3, 4], [4, 5, 6]]
-			for i in order:
-				var next_division = curr_division - 1
-				_create_mesh(corners[i[0] - 1], corners[i[1] - 1], corners[i[2] - 1], next_division)
+func split_unsmooth(mesh: Mesh):
+	var st = SurfaceTool.new()
+	st.begin(Mesh.PRIMITIVE_TRIANGLES)
+	
+	st.append_from(mesh, 0, Transform.IDENTITY)
+	st.generate_normals()
+	
+	mesh.surface_remove(0)
+	st.commit(mesh)
+
+
+func middle_point(point_1: int, point_2: int, cache: Dictionary): 
+	var smaller_index = min(point_1, point_2)
+	var greater_index = max(point_1, point_2)
+	
+	var key = [smaller_index, greater_index]
+	if key in cache:
+		return cache[key]
+	
+	var middle: Vector3 = (0.5 * (vertices[point_1] + vertices[point_2])).normalized()
+	
+	vertices.append(middle)
+	var index = vertices.size() - 1
+	cache[key] = index
+	return index
+
+
+func scale_vertices(rad: float):
+	for i in range(vertices.size()):
+		vertices[i] *= rad
+
+
+func calc_uv_lla(verts: PoolVector3Array) -> PoolVector2Array:
+	var uvs := PoolVector2Array()
+	uvs.resize(verts.size())
+	
+	var uv: Vector2
+	for i in range(verts.size()):
+		uv = xyz_to_lla(verts[i])
+		uv.x = (uv.x / 360.0) + 0.5
+		uv.y = (uv.y / 180.0) + 0.5
+		uvs[i] = uv
+	
+	return uvs
+
+
+func xyz_to_lla(xyz: Vector3) -> Vector2:
+	var lat = asin(xyz.y)
+	var lon = atan2(xyz.z, xyz.x)
+	
+	return Vector2(rad2deg(lon), rad2deg(lat))
+
+
+func lla_to_xyz_arr(arr: PoolVector2Array) -> PoolVector3Array:
+	var verts: PoolVector3Array = PoolVector3Array()
+	verts.resize(arr.size())
+	
+	for i in range(arr.size()):
+		verts[i] = lla_to_xyz(arr[i])
+	
+	return verts
+
+
+func lla_to_xyz(lla: Vector2) -> Vector3:
+	var lon = lla.x
+	var lat = lla.y
+	var cosLat = cos(deg2rad(lat))
+	var sinLat = sin(deg2rad(lat))
+	var cosLon = cos(deg2rad(lon))
+	var sinLon = sin(deg2rad(lon))
+	var x = cosLat * cosLon
+	var y = sinLat
+	var z = cosLat * sinLon
+	return Vector3(x, y, z)
