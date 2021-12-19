0f1d18f78c93db11f45a09f275286bf2149a7346
mlange-42 <martin_lange_@gmx.net>
2021-12-19 17:38:11 +0100

    draw roads as quads instead of lines

--- scripts/debug_draw.gd
+++ scripts/debug_draw.gd
@@ -1,6 +1,9 @@
 extends ImmediateGeometry
 class_name DebugDraw
 
+export var heigt_offset: float = 0.02
+export var road_width: float = 0.05
+
 func draw_path(points: Array, color: Color):
 	clear()
 	begin(Mesh.PRIMITIVE_LINE_STRIP)
@@ -8,14 +11,14 @@ func draw_path(points: Array, color: Color):
 	set_color(color)
 	
 	for p in points:
-		add_vertex(p + 0.1 * p.normalized())
+		add_vertex(p + 2 * heigt_offset * p.normalized())
 	
 	end()
 
 
 func draw_roads(nav: NavManager, roads: RoadNetwork, color1: Color, color2: Color):
 	clear()
-	begin(Mesh.PRIMITIVE_LINES)
+	begin(Mesh.PRIMITIVE_TRIANGLES)
 	
 	for node1 in roads.neighbors:
 		var n = roads.neighbors[node1]
@@ -23,11 +26,18 @@ func draw_roads(nav: NavManager, roads: RoadNetwork, color1: Color, color2: Colo
 		for node2 in n:
 			var edge = roads.edges[[node1, node2]]
 			var p2 = nav.get_node(node2).position
-			var off = (p2 - p1).cross(p1).normalized() * 0.02
+			var x_off = (p2 - p1).cross(p1).normalized() * road_width
+			var y_off = (p2 - p1).normalized() * (0.5 * road_width)
+			var h_off = heigt_offset * p1.normalized()
 			
 			set_color(color1.linear_interpolate(color2, edge.flow/float(edge.capacity)))
-			add_vertex(p1 + 0.02 * p1.normalized() + off)
-			add_vertex(p2 + 0.02 * p2.normalized() + off)
+			add_vertex(p1 + h_off - y_off)
+			add_vertex(p2 + h_off + y_off)
+			add_vertex(p2 + h_off + y_off + x_off)
+			
+			add_vertex(p2 + h_off + y_off + x_off)
+			add_vertex(p1 + h_off - y_off + x_off)
+			add_vertex(p1 + h_off - y_off)
 	
 	end()
 
@@ -39,6 +49,6 @@ func draw_points(nav: NavManager):
 	for data in nav._node_data.values():
 		var p = data.position
 		set_color(Color.blue if data.is_water else Color.wheat)
-		add_vertex(p + 0.025 * p.normalized())
+		add_vertex(p + heigt_offset * p.normalized())
 	
 	end()
