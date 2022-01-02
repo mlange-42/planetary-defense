d5f3bda10297aa236cf501842a5b6afb166da9ed
mlange-42 <martin_lange_@gmx.net>
2022-01-02 20:39:34 +0100

    revert to linear demand dependency

--- scripts/constants/cities.gd
+++ scripts/constants/cities.gd
@@ -8,7 +8,8 @@ const INITIAL_CITY_POP: int = 1
 const NO_PRODUCTS_CITY_POP: int = 3
 
 const PRODUCTS_PER_POP: float = 0.5
-const PRODUCTS_PER_POP_EXP: float = 1.5
 
 static func products_demand(pop: int) -> int:
-	return int(pow(max(pop * PRODUCTS_PER_POP, 1.0) - 1.0, PRODUCTS_PER_POP_EXP))
+	if pop <= NO_PRODUCTS_CITY_POP:
+		return 0
+	return int(max(pop * PRODUCTS_PER_POP, 1.0) - 1.0)
