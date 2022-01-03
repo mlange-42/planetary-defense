3d726af6265f1792bae00d85c6cf4ed88263092a
mlange-42 <martin_lange_@gmx.net>
2022-01-03 01:04:07 +0100

    add supply indicator to air defense facility

--- scripts/objects/air_defense.gd
+++ scripts/objects/air_defense.gd
@@ -1,5 +1,23 @@
 extends Facility
 class_name AirDefense
 
+
+onready var pole: CSGBox = $Pole
+
+var material: SpatialMaterial
+
+func _ready():
+	material = SpatialMaterial.new()
+	material.emission_enabled = true
+	pole.material = material
+
+
+func calc_is_supplied():
+	.calc_is_supplied()
+	var col = Color.green if is_supplied else Color.red
+	pole.material.albedo_color = col
+	pole.material.emission = col
+
+
 func can_build(planet_data, node) -> bool:
 	return not planet_data.get_node(node).is_water
