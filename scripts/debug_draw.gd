dd4bc83a9e9db856bad3441b5f98c52c7e16eb9b
mlange-42 <martin_lange_@gmx.net>
2022-01-06 22:08:17 +0100

    roads vertex-lit instead of unshaded

--- scripts/debug_draw.gd
+++ scripts/debug_draw.gd
@@ -25,14 +25,21 @@ func draw_roads(planet_data, roads: RoadNetwork, color1: Color, color2: Color):
 	for node1 in roads.neighbors:
 		var n = roads.neighbors[node1]
 		var p1 = planet_data.get_position(node1)
+		var norm1 = p1.normalized()
 		for node2 in n:
 			var edge = roads.edges[[node1, node2]]
 			var p2 = planet_data.get_position(node2)
-			var x_off = (p2 - p1).cross(p1).normalized() * Consts.ROAD_WIDTH
-			var y_off = (p2 - p1).normalized() * (0.5 * Consts.ROAD_WIDTH)
+			var direction = (p2 - p1).normalized()
+			var x_dir = direction.cross(p1).normalized()
+			var x_off = x_dir * Consts.ROAD_WIDTH
+			var y_off = direction * (0.5 * Consts.ROAD_WIDTH)
 			var h_off = Consts.DRAW_HEIGHT_OFFSET * p1.normalized()
 			
+			var norm = x_dir.cross(direction)
+			
 			set_color(color1.linear_interpolate(color2, edge.flow/float(edge.capacity)))
+			
+			set_normal(norm)
 			add_vertex(p1 + h_off - y_off)
 			add_vertex(p2 + h_off + y_off)
 			add_vertex(p2 + h_off + y_off + x_off)
