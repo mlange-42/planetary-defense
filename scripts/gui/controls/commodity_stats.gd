b698658b6a304357f630720eec5324ea748ff2c0
mlange-42 <martin_lange_@gmx.net>
2022-01-08 22:22:05 +0100

    fix icon in commodity stats control

--- scripts/gui/controls/commodity_stats.gd
+++ scripts/gui/controls/commodity_stats.gd
@@ -3,14 +3,14 @@ class_name CommodityStats
 
 export (String, "Food", "Resources", "Products") var commodity: String = "Food"
 
-onready var icon = find_node("Icon")
-onready var production = find_node("Production")
-onready var supply = find_node("Supply")
-onready var demand = find_node("Demand")
+onready var icon: TextureRect = find_node("Icon")
+onready var production: Label = find_node("Production")
+onready var supply: Label = find_node("Supply")
+onready var demand: Label = find_node("Demand")
 
 func _ready():
 	icon.hint_tooltip = commodity
-	icon = load(Commodities.COMM_ICONS[commodity])
+	icon.texture = load(Commodities.COMM_ICONS[commodity])
 
 
 func set_values(source: int, moved: int, sink: int):
